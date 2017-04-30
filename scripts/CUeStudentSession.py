15a7c68aa8344a4dba90a6651863c45cb54f967d
Kevin Glasson <kevinglasson@gmail.com>
2017-05-01 00:26:35 +0800

    Begin adding docstrings to all functions / modules

--- scripts/CUeStudentSession.py
+++ scripts/CUeStudentSession.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+"""The CUeStudentSession class and associated functions.
+
+This class contains and stores all functions and variables necessary
+for interacting with Curtin's eStudent portal. It accessess and extracts
+the timetable information for the provided login details and stores them
+in a CurtinUnit object
+
+it contains the following public usable functions: (See the function docstring
+for further information)
+
+class CUeStudentSession(object):
+
+def login(self, studentid, password):
+
+def get_timetables(self):
+
+def get_all_timetables(self):
+"""
+
 from __future__ import print_function
 from bs4 import BeautifulSoup
 from CurtinUnit import CurtinUnit
@@ -19,12 +39,27 @@ DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 
 class LoginFailedError(Exception):
+    """Store the html response code on login exception."""
+
     def __init__(self, response):
+        """Store the respose code in instance variable."""
         self.response = response
 
 
 class CUeStudentSession(object):
+    """Form a requests session for Curtin eStudent.
+
+    This class contains and stores all functions and variables necessary
+    for interacting with Curtin's eStudent portal. It accessess and extracts
+    the timetable information for the provided login details and stores them
+    in a CurtinUnit object
+    """
+
     def __init__(self):
+        """Create the necessary variables for a session.
+
+        Initialises requests session and sets instance variables to defaults
+        """
         self.sess = requests.Session()
         self.timetable_page = None
         # To hold the date of the 'Monday, can figure the rest from that!'
@@ -33,6 +68,16 @@ class CUeStudentSession(object):
 
     # Login
     def login(self, studentid, password):
+        """Log in to Curtin Oasis provided a username and password.
+
+        Navigates to the login page and accesses the page with the provided
+        username and password, raises a LoginFailedError if login was not
+        successful
+
+        Keyword arguments:
+        studentid -- The students id number
+        password -- The students password
+        """
         r = self.sess.get(LOGIN_URL)
         data = dict(UserName=studentid, Password=password)
         r = self.sess.post(LOGIN_URL, data=data, allow_redirects=False)
@@ -46,6 +91,7 @@ class CUeStudentSession(object):
     # Either navigate to the timetable page the first time, or just return the
     # current timetable page
     def get_timetable_page(self):
+        """Navigate to the page with the timetable on it."""
         if not self.timetable_page:
             # Navigate to the 'My Studies' tab
             r = self.sess.post(MY_STUDIES_URL)
@@ -59,6 +105,11 @@ class CUeStudentSession(object):
     # TODO: Implement this, so that we can navigate to a specific date to get
     # the timetable for that week 04-May-2017
     def set_timetable_page_dated(self, date):
+        """Navigate to a timetable of a specific date.
+
+        Keyword arguments:
+        date -- The date to navigate to
+        """
         tt_page = self.timetable_page
         date = from_datetime(date)
         data = make_estudent_happy(tt_page)
@@ -69,10 +120,14 @@ class CUeStudentSession(object):
         r = self.sess.post(TIMETABLE_URL, data=data, allow_redirects=False)
         r.raise_for_status()
         self.timetable_page = r.text
-        self.set_mon_date()
         return r.text
 
     def set_default_date(self):
+        """Set the initial date of the timetable page.
+
+        It is necessary to obtain the first date a different way. After the
+        initial date is established all future dates can be calculated.
+        """
         page = self.timetable_page
         soup = BeautifulSoup(page, "lxml")
         select = soup.find(
@@ -84,13 +139,12 @@ class CUeStudentSession(object):
         date = datetime.datetime.strptime(string[7:], '%b %d, %Y').date()
         self.timetable_page_mon_date = date
 
-    def set_mon_date(self):
-        # page = self.timetable_page
-        # soup = BeautifulSoup(page, "lxml")
-        # date = soup.find(id='ctl00$Content$ctlFilter$TxtStartDt')
-        # print(date)
-        # date = to_datetime(date)
-        # mon_date = date - datetime.timedelta(days=date.weekday())
+    def inc_mon_date(self):
+        """Move the monday date forward 7 days.
+
+        Increment the monday date by 7 days, because the scrape happens in 1
+        week blocks this will set the next monday date to be navigated to
+        """
         print(
             'processed week starting: {}'.format(self.timetable_page_mon_date))
         print('\n')
@@ -98,22 +152,40 @@ class CUeStudentSession(object):
         self.timetable_page_mon_date = mon_date
 
     def get_timetable(self):
+        """Get navigate to the timetable page.
+
+        Process the default timetable page navigated to, this will be the timetable of the week containing todays date, this will only get that
+        timetable, nothing more.
+        """
         self.get_timetable_page()
         return self.proc_timetable_page()
 
     def get_all_timetables(self):
+        """Get all of the timetables for the study period.
+
+        This will process timetables until it finds 3 consectuive empty weeks.
+        This signals the end of that study period and will not break when there
+        is a 2 week tuition free period. Returns the entire list of dictionaries
+        to the caller.
+        """
         sem_unit_lst = []
         self.get_timetable_page()
 
         while self.consecutive_empty_weeks <= 2:
             unit_lst = self.proc_timetable_page()
             self.set_timetable_page_dated(self.timetable_page_mon_date)
+            self.inc_mon_date()
             if self.check_for_empty_week(unit_lst) is False:
                 sem_unit_lst.append(unit_lst)
 
         return (sem_unit_lst)
 
     def check_for_empty_week(self, dict_):
+        """Check if there are only default entries and return True or False.
+
+        Keyword arguments:
+        dict_ -- Dictionary containing the CurtinUnits
+        """
         empty = False
         num_empty = 0
         for key, value in dict_.iteritems():
@@ -129,6 +201,12 @@ class CUeStudentSession(object):
     # This is good, each class can be assigned it's date at this point because
     # it is going day by day. Just need to write a get_date function
     def proc_timetable_page(self):
+        """Process the timetable page to extract the desired information.
+
+        Returns a dict containing the units that were found in the timetable.
+        The key of the dictionary is the unit code and each unit is contained
+        in a CurtinUnit object
+        """
         page = self.timetable_page
         soup = BeautifulSoup(page, "lxml")
         unit_lst = {}
@@ -173,6 +251,11 @@ def make_estudent_happy(page):
 
 
 def to_24h(time):
+    """Convert 12h time string to 24h time string.
+
+    Keyword arguments:
+    time - 12h time in the format 00:00xm
+    """
     am_pm = time[-2:].lower()
     hour, minute = time[:-2].split(':')
     hour = int(hour)
@@ -183,6 +266,13 @@ def to_24h(time):
 
 
 def date_from_day_abbr(string, mon_date):
+    """Convert an abbreviated day string i.e. Wed.
+
+    Requires the date of the monday of that week to calculate the date of the
+    abbreviated day.
+
+    Keyword arguments:
+    """
     for i, day in enumerate(DAYS):
         if day == string:
             date = mon_date + datetime.timedelta(days=i)
@@ -191,6 +281,11 @@ def date_from_day_abbr(string, mon_date):
 
 
 def from_datetime(date):
+    """Take a datetime object and return a compatible string.
+
+    A compatible string is one in the format dd.mm.yyyy. It is necessary for
+    navigating to the required timetable page.
+    """
     month_abbr = ''
     for i, month in enumerate(MONTHS):
         if date.month == i + 1:
@@ -201,6 +296,11 @@ def from_datetime(date):
 
 # 04-May-2017
 def to_datetime(string):
+    """Take a date in string form and return a datetime object.
+
+    Converting date in string format to a datetime object as it is cleaner than
+    storing a string, and easier to access.
+    """
     # datetime.date(2017, 5, 4)
     date = datetime.datetime.strptime(string, '%d-%b-%Y').date()
     return date
